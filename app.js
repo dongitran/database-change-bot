bc7e981e624e1433b27014157d1f946d516ba459
Dong Tran <thiendong.iuh@gmail.com>
2024-03-02 21:24:06 +0700

    add test one table

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var logger = require("morgan");
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
 const { Client } = require("pg");
+const { getDifferences } = require("./functions/get-diff-json");
 
 var app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -19,6 +20,55 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+const client = new Client({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT,
+});
+client.connect();
+
+client.query("LISTEN tbl_customers_change");
+
+client.on("notification", async (msg) => {
+  console.log("Received notification:", msg);
+  const payload = JSON.parse(msg.payload);
+  const action = payload.action;
+  console.log(action, "action");
+  switch (String(action).toUpperCase()) {
+    case "INSERT": {
+      const message = `Có sự thêm mới trong table:\n\`\`\`json\n${JSON.stringify(
+        payload.data,
+        null,
+        2
+      )}\n\`\`\``;
+      await bot.telegram.sendMessage(process.env.TELEGRAM_GROUP_ID, message, {
+        parse_mode: "MarkdownV2",
+      });
+      break;
+    }
+    case "UPDATE": {
+      const newData = payload?.new_data || [];
+      const oldData = payload?.old_data || [];
+      const updateData = {
+        id: payload?.new_data?.id,
+        ...getDifferences(oldData, newData),
+      };
+      const table = (payload?.table_name || "").replace(/_/g, `\\_`);
+      const message = `Update *${table}*:\n\`\`\`json\n${JSON.stringify(
+        updateData,
+        null,
+        2
+      )}\n\`\`\``;
+      await bot.telegram.sendMessage(process.env.TELEGRAM_GROUP_ID, message, {
+        parse_mode: "MarkdownV2",
+      });
+      break;
+    }
+  }
 });
 
 module.exports = app;

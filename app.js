1da9f533f9555294306ac699bbfd10927a717fba
Dong Tran <thiendong.iuh@gmail.com>
2024-03-04 00:41:25 +0700

    add mongo db for watch change

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const logger = require("morgan");
 const { Telegraf } = require("telegraf");
 const fs = require("fs");
+const { MongoClient } = require("mongodb");
 
 const { getDifferences } = require("./functions/get-diff-json");
 const { setupDatabase } = require("./functions/setup-db");
@@ -31,70 +32,130 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// Load database configurations
-const dbConfigs = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "config.json"), "utf-8")
-);
-
 const telegramManager = new TelegramManager(bot, undefined, undefined);
 
 // Database setup and notification handling
-const clients = [];
+const postgresClients = [];
+const mongoClients = [];
 async function processDatabases() {
-  for (const config of dbConfigs) {
-    const client = await setupDatabase(config);
-    clients.push(client);
-  }
+  // Load database configurations
+  const dbConfigs = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "config.json"), "utf-8")
+  );
 
-  for (const client of clients) {
-    client.query("LISTEN tbl_changes");
-
-    client.on("notification", async (msg) => {
-      const payload = JSON.parse(msg.payload);
-      const action = payload.action;
-
-      // Find the config for the database that sent the notification
-      // and send the notification to the Telegram topic
-      const databaseName = payload?.database_name;
-      const config = dbConfigs.find((c) => c.database === databaseName);
-
-      let message = "";
-      switch (String(action).toUpperCase()) {
-        case "INSERT": {
-          const table = (payload?.table_name || "").replace(/_/g, `\\_`);
-          message = `Insert *${table}*:\n\`\`\`json\n${JSON.stringify(
-            sanitizeJson(payload.data),
-            null,
-            2
-          )}\n\`\`\``;
-
-          break;
+  for (const configs of dbConfigs) {
+    switch (configs?.type) {
+      case "postgres": {
+        for (const config of configs?.configs) {
+          const client = await setupDatabase(config);
+          postgresClients.push(client);
         }
-        case "UPDATE": {
-          const newData = payload?.new_data || [];
-          const oldData = payload?.old_data || [];
-          const updateData = {
-            id: payload?.new_data?.id,
-            ...getDifferences(oldData, newData),
-          };
-          const table = (payload?.table_name || "").replace(/_/g, `\\_`);
-          message = `Update *${table}*:\n\`\`\`json\n${JSON.stringify(
-            updateData,
-            null,
-            2
-          )}\n\`\`\``;
 
-          break;
+        for (const client of postgresClients) {
+          client.query("LISTEN tbl_changes");
+
+          client.on("notification", async (msg) => {
+            const payload = JSON.parse(msg.payload);
+            const action = payload.action;
+
+            // Find the config for the database that sent the notification
+            // and send the notification to the Telegram topic
+            const databaseName = payload?.database_name;
+            const config = configs?.configs.find(
+              (c) => c.database === databaseName
+            );
+
+            let message = "";
+            switch (String(action).toUpperCase()) {
+              case "INSERT": {
+                const table = (payload?.table_name || "").replace(/_/g, `\\_`);
+                message = `Insert *${table}*:\n\`\`\`json\n${JSON.stringify(
+                  sanitizeJson(payload.data),
+                  null,
+                  2
+                )}\n\`\`\``;
+
+                break;
+              }
+              case "UPDATE": {
+                const newData = payload?.new_data || [];
+                const oldData = payload?.old_data || [];
+                const updateData = {
+                  id: payload?.new_data?.id,
+                  ...getDifferences(oldData, newData),
+                };
+                const table = (payload?.table_name || "").replace(/_/g, `\\_`);
+                message = `Update *${table}*:\n\`\`\`json\n${JSON.stringify(
+                  updateData,
+                  null,
+                  2
+                )}\n\`\`\``;
+
+                break;
+              }
+            }
+
+            // Append message to telegram manager to send
+            telegramManager.appendMessage(
+              message,
+              configs?.telegramGroupId,
+              config?.messageThreadId
+            );
+          });
         }
+        break;
       }
+      case "mongo": {
+        for (const config of configs?.configs) {
+          const client = new MongoClient(config.uri);
+          await client.connect();
+          mongoClients.push(client);
 
-      // Append message to telegram manager to send
-      telegramManager.appendMessage(
-        message,
-        process.env.TELEGRAM_GROUP_ID,
-        config?.messageThreadId
-      );
-    });
+          const database = client.db(config.database);
+          const changeStream = database.watch();
+
+          changeStream.on("change", (change) => {
+            const operationType = change.operationType;
+            const fullDocument = change.fullDocument;
+            const ns = change.ns;
+            const collectionName = (ns.coll || "").replace(/_/g, `\\_`);
+
+            let message;
+            switch (operationType) {
+              case "insert": {
+                message = `Insert on *${collectionName}*:\n\`\`\`json\n${JSON.stringify(
+                  sanitizeJson(fullDocument),
+                  null,
+                  2
+                )}\n\`\`\``;
+                break;
+              }
+              case "update": {
+                const updateFields = change?.updateDescription?.updatedFields;
+                const objResponse = {
+                  _id: change?.documentKey?._id,
+                  ...updateFields,
+                };
+                message = `Update on *${collectionName}*:\n\`\`\`json\n${JSON.stringify(
+                  sanitizeJson(objResponse),
+                  null,
+                  2
+                )}\n\`\`\``;
+                break;
+              }
+            }
+
+            // Append message to telegram manager to send
+            telegramManager.appendMessage(
+              message,
+              configs?.telegramGroupId,
+              config?.messageThreadId
+            );
+          });
+        }
+        break;
+      }
+    }
   }
 }
 
@@ -103,7 +164,7 @@ processDatabases().catch(console.error);
 
 // Schedule the telegram bot to send a message every 1 seconds
 scheduleJob("*/1 * * * * *", async function () {
-  telegramManager.sendOneMessage(true, process.env.TELEGRAM_GROUP_ID);
+  telegramManager.sendOneMessage(true);
 });
 
 module.exports = app;
--- functions/telegram-manager.js
+++ functions/telegram-manager.js
@@ -7,24 +7,28 @@ class TelegramManager {
     this.messageId = null;
     this.messageCurrent = [];
     this.timeCheckSendMessage = new Date().getTime();
+    this.processing = false;
   }
 
   async appendMessage(message, chatId, messageThreadId) {
     let hasSameMessage = false;
-    this.messageCurrent = this.messageCurrent.reverse().map((messageObj) => {
-      if (
-        !hasSameMessage &&
-        messageObj.chatId === chatId &&
-        messageObj.messageThreadId === messageThreadId && 
-        (messageObj.message + message).length < 4096
-      ) {
-        hasSameMessage = true;
-        return {
-          ...messageObj,
-          message: messageObj.message + message,
-        };
-      } else return messageObj;
-    }).reverse();
+    this.messageCurrent = this.messageCurrent
+      .reverse()
+      .map((messageObj) => {
+        if (
+          !hasSameMessage &&
+          messageObj.chatId === chatId &&
+          messageObj.messageThreadId === messageThreadId &&
+          (messageObj.message + message).length < 4096
+        ) {
+          hasSameMessage = true;
+          return {
+            ...messageObj,
+            message: messageObj.message + message,
+          };
+        } else return messageObj;
+      })
+      .reverse();
 
     if (!hasSameMessage) {
       this.messageCurrent.push({
@@ -36,6 +40,8 @@ class TelegramManager {
   }
 
   async sendOneMessage(checkTime) {
+    // Check if processing
+    if (this.processing) return;
     // Check if message is empty to return
     if (this.messageCurrent.length === 0) return;
 
@@ -47,6 +53,9 @@ class TelegramManager {
       }
       this.timeCheckSendMessage = now;
     }
+
+    // Set the processing
+    this.processing = true;
 
     // Get the first message
     const messageObj = this.messageCurrent[0];
@@ -64,8 +73,14 @@ class TelegramManager {
       // Remove the first message
       this.messageCurrent.shift();
 
+      // Clear the processing
+      this.processing = false;
+
       return t;
     } catch (error) {
+      // Clear the processing
+      this.processing = false;
+
       console.log("send message current error: ", error);
     }
   }

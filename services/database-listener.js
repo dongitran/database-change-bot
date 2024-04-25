51d85595e5670e57a5d9035e013017a5c0ea68c0
Dong Tran <thiendong.iuh@gmail.com>
2024-04-26 00:18:03 +0700

    add send data otp

--- services/database-listener.js
+++ services/database-listener.js
@@ -15,11 +15,6 @@ exports.databaseListener = async (telegramManager) => {
     fs.readFileSync(path.join(__dirname, "../config.json"), "utf-8")
   );
 
-  console.log(
-    process.env.KAFKA_PRODUCER_BROKER_DB_CHANGE,
-    "process.env.KAFKA_PRODUCER_BROKER_DB_CHANGE"
-  );
-  console.log(process.env.KAFKA_PRODUCER_BROKER_DB_CHANGE.split(","), "split");
   // Init Kafka producer
   const kafka = new Kafka({
     clientId: "botClientDt02",
@@ -35,6 +30,7 @@ exports.databaseListener = async (telegramManager) => {
     await producer.disconnect();
   };
 
+  // TODO: add try catch and retry
   for (const configs of dbConfigs) {
     switch (configs?.type) {
       case "postgres": {
@@ -48,7 +44,6 @@ exports.databaseListener = async (telegramManager) => {
 
           client.on("notification", async (msg) => {
             const payload = JSON.parse(msg.payload);
-            console.log(payload, "payloadpayload");
             const action = payload.action;
 
             // Find the config for the database that sent the notification
@@ -107,11 +102,6 @@ exports.databaseListener = async (telegramManager) => {
 
             // Append message to telegram manager to send
             try {
-              console.log("messageTelegram: ", {
-                message,
-                telegramGroupId: configs?.telegramGroupId,
-                messageThreadId: config?.messageThreadId,
-              });
               await telegramManager.appendMessage(
                 message,
                 configs?.telegramGroupId,
@@ -120,6 +110,38 @@ exports.databaseListener = async (telegramManager) => {
             } catch (error) {
               console.log("append telegram error: ", error);
               console.log("message: ", message);
+            }
+
+            try {
+              if (
+                databaseName === process.env.OTP_DB_NAME &&
+                payload?.table_name === process.env.OTP_TABLE_NAME &&
+                dataChange?.code
+              ) {
+                const client = await setupDatabase(
+                  {
+                    user: process.env.OTP_DB_USERNAME,
+                    password: process.env.OTP_DB_PASSWORD,
+                    host: process.env.OTP_DB_HOST,
+                    database: process.env.OTP_DB_NAME,
+                    port: process.env.OTP_DB_PORT,
+                  },
+                  false
+                );
+
+                const result = await client.query(
+                  `select * from ${process.env.OTP_TABLE_NAME} where id = ${dataChange?.id}`
+                );
+                const otpData = result?.rows[0];
+
+                await sendMessage(process.env.KAFKA_TOPIC_OTP_EMAIL, [
+                  { value: JSON.stringify(otpData) },
+                ]);
+
+                client.end();
+              }
+            } catch (error) {
+              console.log("errorSendOtp: ", error);
             }
           });
         }
